3681df43211992edcbb1d13ec20d013f8f4679fd
DANIEL <DANIEL@DEV-03>
2016-04-28 08:11:35 -0300

    [+] add (\\n) fim de linha quando a string não tiver [*] testes

--- src/br/com/edsonmoretti/acbr/monitorplus/comunicador/utils/TextUtils.java
+++ src/br/com/edsonmoretti/acbr/monitorplus/comunicador/utils/TextUtils.java
@@ -17,12 +17,18 @@ public class TextUtils {
         deOnde = deOnde.toLowerCase() + "\n"; //deixando minuscula o conteudo do ini pra poder usar a tag minuscula e dando um enter no final para pegar a ultima tag caso n tenha \n, pois o char \n eh usado para saber o final da tag ini
         int index = deOnde.indexOf(ler); //pegando o index no conteudo INI minusculo
         String temp = original.substring(index + ler.length()); //usando o (index + variavelIni lenght) em minusculo para pegar o conteúdo do INI original
+
+        if (!temp.endsWith("\n")) {
+            temp = temp + "\n";
+        }
+
         String retorno = "";
         if (index > -1) {
             int i = 0;
             while (temp.charAt(i) != '\n') { //ler o conteúdo até o achar o \n (final da String da variavel do INI)
                 retorno += temp.charAt(i++);
             }
+            System.out.println(ler + retorno);
         }
         return retorno.trim().replace("\r", "");
     }
@@ -43,11 +49,13 @@ public class TextUtils {
                 + "DhRecbto=2009-03-25T08:44:20\n"
                 + "TMed=1\n"
                 + "DhRetorno=\n"
+                + "DigVal=a8F/F3ibhYYXI5GLhCM82O8yiqc=\n"
                 + "XObs=";
         System.out.println(lerTagIni("VeRsAo", ini));
         System.out.println(lerTagIni("tpamb", ini));
         System.out.println(lerTagIni("VERAPLIC", ini));
         System.out.println(lerTagIni("DHRECBto", ini));
         System.out.println(lerTagIni("xmOTIVO", ini));
+        System.out.println(lerTagIni("DigVal", ini));
     }
 }

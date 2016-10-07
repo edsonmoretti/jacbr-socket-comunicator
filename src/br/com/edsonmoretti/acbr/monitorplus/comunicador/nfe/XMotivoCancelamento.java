a6c1386c78d5a6ec8c9045d476978de6d2edf69d
DANIEL <DANIEL@DEV-03>
2016-10-07 11:58:33 -0300

    [*]

--- src/br/com/edsonmoretti/acbr/monitorplus/comunicador/nfe/XMotivoCancelamento.java
+++ src/br/com/edsonmoretti/acbr/monitorplus/comunicador/nfe/XMotivoCancelamento.java
@@ -9,15 +9,8 @@ package br.com.edsonmoretti.acbr.monitorplus.comunicador.nfe;
  *
  * @author Edson
  */
-public class XMotivoCancelamento extends XMotivo {
+public class XMotivoCancelamento extends XMotivoEvento {
 
-    private String ChNFe;
-    private String NProt;
-    private String tpEvento;
-    private String xEvento;
-    private String nSeqEvento;
-    private String CNPJDest;
-    private String emailDest;
     private String XML;
 
     public XMotivoCancelamento(String re) {
@@ -26,62 +19,6 @@ public class XMotivoCancelamento extends XMotivo {
 
     public XMotivoCancelamento(XMotivo re) {
         super(re);
-    }
-
-    public String getChNFe() {
-        return ChNFe;
-    }
-
-    public void setChNFe(String ChNFe) {
-        this.ChNFe = ChNFe;
-    }
-
-    public String getNProt() {
-        return NProt;
-    }
-
-    public void setNProt(String NProt) {
-        this.NProt = NProt;
-    }
-
-    public String getTpEvento() {
-        return tpEvento;
-    }
-
-    public void setTpEvento(String tpEvento) {
-        this.tpEvento = tpEvento;
-    }
-
-    public String getxEvento() {
-        return xEvento;
-    }
-
-    public void setxEvento(String xEvento) {
-        this.xEvento = xEvento;
-    }
-
-    public String getnSeqEvento() {
-        return nSeqEvento;
-    }
-
-    public void setnSeqEvento(String nSeqEvento) {
-        this.nSeqEvento = nSeqEvento;
-    }
-
-    public String getCNPJDest() {
-        return CNPJDest;
-    }
-
-    public void setCNPJDest(String CNPJDest) {
-        this.CNPJDest = CNPJDest;
-    }
-
-    public String getEmailDest() {
-        return emailDest;
-    }
-
-    public void setEmailDest(String emailDest) {
-        this.emailDest = emailDest;
     }
 
     public String getXML() {
@@ -94,7 +31,7 @@ public class XMotivoCancelamento extends XMotivo {
 
     @Override
     public String toString() {
-        return "XMotivoCancelamento{" + "ChNFe=" + ChNFe + ", NProt=" + NProt + ", tpEvento=" + tpEvento + ", xEvento=" + xEvento + ", nSeqEvento=" + nSeqEvento + ", CNPJDest=" + CNPJDest + ", emailDest=" + emailDest + ", XML=" + XML + " - " + super.toString();
+        return "XMotivoCancelamento{" + "XML=" + XML + " - " + super.toString() + '}';
     }
 
 }
